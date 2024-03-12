91335d3405510bb5f945789cc8b3e4bc65cdd6e7
seupaidando <163136726+seupaidando@users.noreply.github.com>
2024-03-12 09:43:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/736x/49/0b/ca/490bca9575c557b0e72469f25c87d388.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://pbs.twimg.com/media/FcfKIZIWIAAlNNp?format=jpg&name=4096x4096";
+
+}
